574bd8c62e188abbb67fd1f87c41f8e8c37c6f2f
uruhara91 <79046614+uruhara91@users.noreply.github.com>
2026-01-01 23:40:59 +0700

    Strict the timing

--- QtScrcpy/QtScrcpyCore/src/device/decoder/decoder.cpp
+++ QtScrcpy/QtScrcpyCore/src/device/decoder/decoder.cpp
@@ -81,10 +81,11 @@ bool Decoder::open()
         return false;
     }
 
-   //_codecCtx->flags |= AV_CODEC_FLAG_LOW_DELAY;
-   //_codecCtx->flags2 |= AV_CODEC_FLAG2_FAST;
-   //_codecCtx->thread_type = FF_THREAD_SLICE;
-   //_codecCtx->thread_count = 2;
+//  m_codecCtx->flags |= AV_CODEC_FLAG_LOW_DELAY;
+//  m_codecCtx->flags2 |= AV_CODEC_FLAG2_FAST;
+    m_codecCtx->extra_hw_frames = 1;
+//  m_codecCtx->thread_type = FF_THREAD_SLICE;
+//  m_codecCtx->thread_count = 2;
 
     if (!initHWDecoder(codec)) {
         qWarning("VAAPI init failed, falling back to software decoding.");
